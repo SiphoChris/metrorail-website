5969aa93e6aacec45417bdfe1b362696e185eed9
sinkecpt025 <sinkecpt025@student.wethinkcode.co.za>
2026-03-25 10:08:58 +0200

    fix: the convex provider to lazy load it after module instantiation

--- src/integrations/convex/provider.tsx
+++ src/integrations/convex/provider.tsx
@@ -2,16 +2,19 @@ import { ConvexProvider } from 'convex/react'
 import { ConvexQueryClient } from '@convex-dev/react-query'
 
 const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL
-if (!CONVEX_URL) {
-  console.error('missing envar CONVEX_URL')
-}
-const convexQueryClient = new ConvexQueryClient(CONVEX_URL)
+
+const convexQueryClient =
+  typeof window !== 'undefined' && CONVEX_URL
+    ? new ConvexQueryClient(CONVEX_URL)
+    : null
 
 export default function AppConvexProvider({
   children,
 }: {
   children: React.ReactNode
 }) {
+  if (!convexQueryClient) return <>{children}</>
+
   return (
     <ConvexProvider client={convexQueryClient.convexClient}>
       {children}
