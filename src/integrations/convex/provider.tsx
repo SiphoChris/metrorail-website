6ae070417f4d9127a5a01a28c684910ba37ccbfe
sinkecpt025 <sinkecpt025@student.wethinkcode.co.za>
2026-03-26 16:00:57 +0200

    chore(cleanup): remove unsed code in the provider and the root route

--- src/integrations/convex/provider.tsx
+++ src/integrations/convex/provider.tsx
@@ -1,27 +1,3 @@
-// import { ConvexProvider } from 'convex/react'
-// import { ConvexQueryClient } from '@convex-dev/react-query'
-
-// const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL
-
-// const convexQueryClient =
-//   typeof window !== 'undefined' && CONVEX_URL
-//     ? new ConvexQueryClient(CONVEX_URL)
-//     : null
-
-// export default function AppConvexProvider({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   if (!convexQueryClient) return <>{children}</>
-
-//   return (
-//     <ConvexProvider client={convexQueryClient.convexClient}>
-//       {children}
-//     </ConvexProvider>
-//   )
-// }
-
 import { ConvexProvider, ConvexReactClient } from 'convex/react'
 
 const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL
