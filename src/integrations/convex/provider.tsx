9eb9ef936ccfed273c6bbed6057d37d1f5c05b1f
sinkecpt025 <sinkecpt025@student.wethinkcode.co.za>
2026-03-26 15:46:54 +0200

    fix: convex provider

--- src/integrations/convex/provider.tsx
+++ src/integrations/convex/provider.tsx
@@ -1,40 +1,37 @@
-import { ConvexProvider } from 'convex/react'
-import { ConvexQueryClient } from '@convex-dev/react-query'
+// import { ConvexProvider } from 'convex/react'
+// import { ConvexQueryClient } from '@convex-dev/react-query'
 
-const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL
+// const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL
 
-const convexQueryClient =
-  typeof window !== 'undefined' && CONVEX_URL
-    ? new ConvexQueryClient(CONVEX_URL)
-    : null
-
-export default function AppConvexProvider({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  if (!convexQueryClient) return <>{children}</>
-
-  return (
-    <ConvexProvider client={convexQueryClient.convexClient}>
-      {children}
-    </ConvexProvider>
-  )
-}
-
-
-// import { ConvexProvider, ConvexReactClient } from 'convex/react'
-
-// const CONVEX_URL = import.meta.env.VITE_CONVEX_URL
-
-// const convex = CONVEX_URL ? new ConvexReactClient(CONVEX_URL) : null
+// const convexQueryClient =
+//   typeof window !== 'undefined' && CONVEX_URL
+//     ? new ConvexQueryClient(CONVEX_URL)
+//     : null
 
 // export default function AppConvexProvider({
 //   children,
 // }: {
 //   children: React.ReactNode
 // }) {
-//   if (!convex) return <>{children}</>
+//   if (!convexQueryClient) return <>{children}</>
 
-//   return <ConvexProvider client={convex}>{children}</ConvexProvider>
+//   return (
+//     <ConvexProvider client={convexQueryClient.convexClient}>
+//       {children}
+//     </ConvexProvider>
+//   )
 // }
+
+import { ConvexProvider, ConvexReactClient } from 'convex/react'
+
+const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL
+const convex = CONVEX_URL ? new ConvexReactClient(CONVEX_URL) : null
+
+export default function AppConvexProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  if (!convex) return <>{children}</>
+  return <ConvexProvider client={convex}>{children}</ConvexProvider>
+}
